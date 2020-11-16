5a99cca18941763b335701c5519471cafdc963e1
Mujtaba kamaL rahseed <74490076+Mujtaba-rasheed@users.noreply.github.com>
2020-11-16 16:29:53 +0200

    Add files via upload

---
+++ pageالبيانات الاساسية.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+<title>My first PHP page</title>
+</head>
+<body bgcolor="#00000">
+<?php 
+
+echo<h1> <h2 align="center"><font color='white'> بسم الله الرحمن الرحيم<h2/></h1>
+   <h1> <h2 align="center"> كلية الامـارات للعلوم والتكنولوجيا<h2/></h1>
+      <h1> <h2 align="center"> قسـم : تقنية المعلومات <h2/></h1>
+	     <h1> <h2 align="center">  البيانـــات الاسـاسية<h2/></h1>
+		    <h1> <h2 align="center">  أعــداد : الطالب مجتبي كمال رشيد<h2/></h1>
+			   <h1> <h2 align="center">  أشــــراف : د / محمد عثمان<h2/></h1>
+			 
+			   <h1 align="center"><font color="#fff3fffff3fffff3fffff3fffff3fffff3fffff3ff">
+ <span><table border = 20 width="500"height="70"bgcolor="#090050"align="center"> 
+<tr>
+<td><center> <font color='yellow'><h1>البيانات الاســاسية  </center></td>
+</tr> 
+   <table border=  26 width="570"height="70"bgcolor="#5b0002"align="center">
+ 
+<caption/> <caption> 
+ <thead>
+ <tr>
+<th/><font color=' white'> مجتبي كمال رشيد محمد<th> 
+ <th><font color='yellow'>:الاسم</th>
+ </tr>
+ </thead> 
+<thead>
+ <tr>
+<th/><font color=' white'> الخرطوم / تـوتي<th> 
+ <th><font color='yellow'>:السكن</th>
+ </tr>
+ </thead>
+ <tr>
+<th/><font color=' white'>95682<th> 
+ <th><font color='yellow'>  : رقم المنزل</th>
+ </tr>
+ </thead>
+     <thead>
+ <tr>
+<th/><font color=' white'>0922951870<th> 
+ <th><font color='yellow'> :رقم الجوال</th>
+ </tr>
+ </thead>
+     <thead>
+ <tr>
+<th/><font color=' white'>Mujtaba_459@outlok.com<th> 
+ <th><font color='yellow'> :البريد الالكتروني</th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'>طالب جامعي<th> 
+ <th><font color='yellow'> :الوظيفة</th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'>كلية الامارات للعلوم والتكنولوجيا<th> 
+ <th><font color='yellow'>:أسم الجامعة</th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'>تقنية المعلومات<th> 
+ <th><font color='yellow'> :التخصص</th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'>الخامس<th> 
+ <th><font color='yellow'>:الفصل</th>
+ </tr>
+ </thead>
+ 
+</body>         
+</html>
